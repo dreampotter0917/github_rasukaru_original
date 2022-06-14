fa09437721cc3ab04aa089da3108b40556f741f4
yume arai <dreampotter.0917@icloud.com>
2022-06-14 22:03:38 +0900

    june forty

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
 $(function () {
 
-$(function () {
-   // スクロールしたときに実行
-   $(window).scroll(function () {
-      // 目的のスクロール量を設定(px)
-      var TargetPos = 450;
-      // 現在のスクロール位置を取得
-      var ScrollPos = $(window).scrollTop();
-      // 現在位置が目的のスクロール量に達しているかどうかを判断
-      if (ScrollPos >= TargetPos) {
-         // 達していれば表示
-         $("#topbutton").fadeIn();
-      } else {
-         // 達していなければ非表示
-         $("#topbutton").fadeOut();
-      }
+   $(function () {
+      // スクロールしたときに実行
+      $(window).scroll(function () {
+         // 目的のスクロール量を設定(px)
+         var TargetPos = 450;
+         // 現在のスクロール位置を取得
+         var ScrollPos = $(window).scrollTop();
+         // 現在位置が目的のスクロール量に達しているかどうかを判断
+         if (ScrollPos >= TargetPos) {
+            // 達していれば表示
+            $("#topbutton").fadeIn();
+         } else {
+            // 達していなければ非表示
+            $("#topbutton").fadeOut();
+         }
+      });
    });
-});
 
 
-$(function () {
-   $('#toggle').on('click', function () {
-      $('body').toggleClass('is-open');
+   $(function () {
+      $('#toggle').on('click', function () {
+         $('body').toggleClass('is-open');
+      });
    });
-});
 
 
-$(function () {
-   $('nav a').on('click', function () {
-      $('body').removeClass('is-open');
+   $(function () {
+      $('nav a').on('click', function () {
+         $('body').removeClass('is-open');
+      });
    });
+
+
+   $(".modal-open").modaal()
+
+   $(function () {
+      $('.button').prevAll().hide();
+      $('.button').click(function () {
+          if ($(this).prevAll().is(':hidden')) {
+              $(this).prevAll().slideDown();
+              $(this).text('閉じる').addClass('close');
+          } else {
+              $(this).prevAll().slideUp();
+              $(this).text('もっと見る').removeClass('close');
+          }
+      });
+  });
+
 });
-
-
-$(".modal-open").modaal()
-
-});
-
-
-
-
